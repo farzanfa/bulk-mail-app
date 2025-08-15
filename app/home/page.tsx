cebc1b23f7e8d4c78638d24d1f7d139347690181
Farzan F A <farzanfa07@gmail.com>
2025-08-15 19:28:48 +0530

    home: boost background animations (opacity, blur, sizes, will-change)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,9 +3,9 @@ import HomeClient from './HomeClient';
 export default function HomePage() {
   return (
     <div className="relative overflow-hidden">
-      <div className="pointer-events-none absolute inset-0 bg-dot-grid z-0"></div>
-      <div className="pointer-events-none absolute top-[-10%] left-[-10%] h-[24rem] w-[24rem] rounded-full bg-yellow-200 blur-3xl opacity-50 animate-float z-0"></div>
-      <div className="pointer-events-none absolute bottom-[-12%] right-[-12%] h-[28rem] w-[28rem] rounded-full bg-yellow-100 blur-3xl opacity-50 animate-float z-0" style={{ animationDelay: '-6s' }}></div>
+      <div className="pointer-events-none absolute inset-0 bg-dot-grid z-0" />
+      <div className="pointer-events-none absolute -top-24 -left-24 h-[28rem] w-[28rem] rounded-full bg-yellow-200 blur-[80px] opacity-60 animate-float z-0" />
+      <div className="pointer-events-none absolute -bottom-28 -right-28 h-[30rem] w-[30rem] rounded-full bg-yellow-100 blur-[90px] opacity-60 animate-float z-0" style={{ animationDelay: '-6s' }} />
       <div className="relative z-10">
         <HomeClient />
       </div>
