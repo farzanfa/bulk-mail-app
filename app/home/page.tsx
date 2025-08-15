6081103a5b5351f3c5462f74c4821723b6f6b4ec
Farzan F A <farzanfa07@gmail.com>
2025-08-15 18:47:18 +0530

    home: add animated dot-grid and floating blobs background

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,14 @@
 import HomeClient from './HomeClient';
 
 export default function HomePage() {
-  return <HomeClient />;
+  return (
+    <div className="relative">
+      <div className="pointer-events-none absolute inset-0 bg-dot-grid"></div>
+      <div className="pointer-events-none absolute -top-12 -left-12 h-48 w-48 rounded-full bg-yellow-200 blur-3xl opacity-40 animate-float"></div>
+      <div className="pointer-events-none absolute -bottom-12 -right-12 h-48 w-48 rounded-full bg-yellow-100 blur-3xl opacity-40 animate-float" style={{ animationDelay: '-6s' }}></div>
+      <HomeClient />
+    </div>
+  );
 }
 
 
